73ada1a08b9131d0b01aef00984c00f477e3da3e
Watcharaphong <65011212077@msu.ac.th>
2024-08-19 03:09:15 +0700

    add index page

--- api/index.ts
+++ api/index.ts
@@ -3,3 +3,7 @@ import { conn, queryAsync } from "../dbconnect";
 import mysql from "mysql";
 
 export const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("Hello world");
+});
--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express, { response } from "express";
 import { conn } from "./dbconnect";
 import cors from "cors";
 import { router as user } from './api/user';
+import { router as index } from "./api/index";
 import bodyParser from "body-parser";
 
 export const app = express();
@@ -9,7 +10,8 @@ export const app = express();
 
 app.use(bodyParser.text());
 app.use(bodyParser.json());
-app.use("/", (req, res) => {
-    res.send("Hello world");
-})
+// app.use("/", (req, res) => {
+//     res.send("Hello world");
+// })
+app.use("/", index)
 app.use("/user", user);
