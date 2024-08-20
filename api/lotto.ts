ec9703537ff0b82c83b5feb4eb6642a090852955
Watcharaphong <65011212077@msu.ac.th>
2024-08-20 16:21:17 +0700

    add update win lotto

--- api/lotto.ts
+++ api/lotto.ts
@@ -7,76 +7,118 @@ export const router = express.Router();
 
 // get all lotto
 router.get("/", (req, res) => {
-    let sql = "SELECT * FROM lotto";
+  let sql = "SELECT * FROM lotto";
 
-    conn.query(sql, (err, result) => {
-        if(err){
-            res.status(400).json({
-                response: false,
-                message: "Failed to load data"
-            })
-        }
-        if(result != ""){
-            res.status(200).json({
-                response: true,
-                result
-            })
-        }
-    })
-})
+  conn.query(sql, (err, result) => {
+    if (err) {
+      res.status(400).json({
+        response: false,
+        message: "Failed to load data",
+      });
+    }
+    if (result != "") {
+      res.status(200).json({
+        response: true,
+        result,
+      });
+    }
+  });
+});
 
 // insert 100 lotto from random at admin
 router.post("/", (req, res) => {
-    let lottoNum = req.body.numbers;
-    let value =     lottoNum.map((num: any) => [num]);
+  let lottoNum = req.body.numbers;
+  let value = lottoNum.map((num: any) => [num]);
 
-    let sql = "INSERT INTO lotto (number) VALUES ?";
-    sql = mysql.format(sql, [
-        value
-    ])
+  let sql = "INSERT INTO lotto (number) VALUES ?";
+  sql = mysql.format(sql, [value]);
 
-    conn.query(sql, (err, result) => {
-        if(err) throw err;
-        res.status(200).json({response: true, message: "Number has been updated"})
-    })
-})
+  conn.query(sql, (err, result) => {
+    if (err) throw err;
+    res
+      .status(200)
+      .json({ response: true, message: "Number has been updated" });
+  });
+});
 
 // select lotto that not has been sell
 router.get("/notsell", (req, res) => {
-    let sql = "SELECT * FROM lotto WHERE sell = 0";
+  let sql = "SELECT * FROM lotto WHERE sell = 0";
 
-    conn.query(sql, (err, result) => {
-        if(err) throw err;
-        res.status(200).json({response: true, result});
-    })
-})
+  conn.query(sql, (err, result) => {
+    if (err) throw err;
+    res.status(200).json({ response: true, result });
+  });
+});
 
 // select lotto that already sell
 router.get("/sell", (req, res) => {
-    let sql = "SELECT * FROM lotto WHERE sell != 0";
+  let sql = "SELECT * FROM lotto WHERE sell != 0";
 
-    conn.query(sql, (err, result) => {
-        if(err) throw err;
-        res.status(200).json({response: true, result});
-    })
-})
+  conn.query(sql, (err, result) => {
+    if (err) throw err;
+    res.status(200).json({ response: true, result });
+  });
+});
 
 // random number from all lotto
 router.get("/jackpotall", (req, res) => {
-    let sql = "SELECT * FROM lotto WHERE sell = 0 ORDER BY RAND() LIMIT 5";
+  let sql = "SELECT * FROM lotto WHERE sell = 0 ORDER BY RAND() LIMIT 5";
 
-    conn.query(sql, (err, result) => {
-        if(err) throw err;
-        res.status(200).json({response: true, result});
-    })
-})
+  conn.query(sql, (err, result) => {
+    if (err) throw err;
+    res.status(200).json({ response: true, result });
+  });
+});
 
 // random number from sell lotto
 router.get("/jackpotsell", (req, res) => {
-    let sql = "SELECT * FROM lotto WHERE sell != 0 ORDER BY RAND() LIMIT 5";
+  let sql = "SELECT * FROM lotto WHERE sell != 0 ORDER BY RAND() LIMIT 5";
 
-    conn.query(sql, (err, result) => {
-        if(err) throw err;
-        res.status(200).json({response: true, result});
-    })
-})
+  conn.query(sql, (err, result) => {
+    if (err) throw err;
+    res.status(200).json({ response: true, result });
+  });
+});
+
+// update Jackpot lotto from all lotto
+router.put("/jackpotall", (req, res) => {
+  let lottoNum = req.body.numbers;
+  let value = lottoNum.map((num: any) => [num]);
+
+  let sql = "UPDATE lotto SET win = ? WHERE number = ?";
+
+  let promises = lottoNum.map((num: any) => {
+    let formattedSql = mysql.format(sql, [1, num]);
+
+    // Return a promise for each query
+    return new Promise((resolve, reject) => {
+      conn.query(formattedSql, (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      });
+    });
+  });
+  res.status(200).json({response: true})
+});
+
+// update Jackpot lotto from only lotto that has been sell
+router.put("/jackpotsell", (req, res) => {
+let lottoNum = req.body.numbers;
+  let value = lottoNum.map((num: any) => [num]);
+
+  let sql = "UPDATE lotto SET win = ? WHERE number = ? AND sell != ?";
+
+  let promises = lottoNum.map((num: any) => {
+    let formattedSql = mysql.format(sql, [1, num, 0]);
+
+    // Return a promise for each query
+    return new Promise((resolve, reject) => {
+      conn.query(formattedSql, (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      });
+    });
+  });
+  res.status(200).json({response: true})
+  });
