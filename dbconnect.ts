5d3430f70ad0146250f4da0436a120eed870827d
Watcharaphong <65011212077@msu.ac.th>
2024-09-03 14:07:05 +0700

    change to AJ database

--- dbconnect.ts
+++ dbconnect.ts
@@ -1,12 +1,20 @@
 import mysql from "mysql";
 import util from "util";
 
+// export const conn = mysql.createPool({
+//     connectionLimit: 10,
+//     host: "mysql-lottomuu.alwaysdata.net",
+//     user: "lottomuu",
+//     password: "lotto123",
+//     database: "lottomuu_database",
+// })
+
 export const conn = mysql.createPool({
     connectionLimit: 10,
-    host: "mysql-lottomuu.alwaysdata.net",
-    user: "lottomuu",
-    password: "lotto123",
-    database: "lottomuu_database",
+    host: "202.28.34.197",
+    user: "web66_65011212077",
+    password: "65011212077@csmsu",
+    database: "web66_65011212077",
 })
 // export const conn = mysql.createPool({
 //     connectionLimit: 10,
@@ -15,6 +23,8 @@ export const conn = mysql.createPool({
 //     password: "rkpi3VbjuF",
 //     database: "sql12728153",
 // })
+
+
 
 export const queryAsync = util.promisify(conn.query).bind(conn);
 
