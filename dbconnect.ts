c3b7ec64ff652deeca133e523eee9930311a57cd
torikiton <65011212154@msu.ac.th>
2024-09-13 18:38:37 +0700

    change to alwaysdata database

--- dbconnect.ts
+++ dbconnect.ts
@@ -1,21 +1,21 @@
 import mysql from "mysql";
 import util from "util";
 
-// export const conn = mysql.createPool({
-//     connectionLimit: 10,
-//     host: "mysql-lottomuu.alwaysdata.net",
-//     user: "lottomuu",
-//     password: "lotto123",
-//     database: "lottomuu_database",
-// })
-
 export const conn = mysql.createPool({
     connectionLimit: 10,
-    host: "202.28.34.197",
-    user: "web66_65011212077",
-    password: "65011212077@csmsu",
-    database: "web66_65011212077",
+    host: "mysql-lottomuu.alwaysdata.net",
+    user: "lottomuu",
+    password: "lotto123",
+    database: "lottomuu_database",
 })
+
+// export const conn = mysql.createPool({
+//     connectionLimit: 10,
+//     host: "202.28.34.197",
+//     user: "web66_65011212077",
+//     password: "65011212077@csmsu",
+//     database: "web66_65011212077",
+// })
 // export const conn = mysql.createPool({
 //     connectionLimit: 10,
 //     host: "sql12.freemysqlhosting.net",
